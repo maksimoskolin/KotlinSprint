9a09b15810719f2a5ebc8a59c5c1f06f66275558
maksimmilashenko <maximlepolepo777@gmail.com>
2025-02-14 15:58:34 +0300

    KS-Task1-3

---
+++ src/lesson_1/task1_3.kt
@@ -0,0 +1,14 @@
+package lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("Год полёта - $year")
+    println("Взлёт - $hour:$minute")
+
+    hour = "10"
+    minute = "55"
+    println("Посадка - $hour:$minute")
+}
